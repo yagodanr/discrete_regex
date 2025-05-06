9a0005a47af89fbb38f6fd1e61e819003536d721
yagodanr <yagodanr@gmail.com>
2025-05-06 15:36:14 +0300

    test module

---
+++ test_regex.py
@@ -0,0 +1,78 @@
+from unittest import TestCase
+from regex import RegexFSM
+
+class TestRegexFSM(TestCase):
+
+
+    def test_simple_pattern0(self):
+        pattern = "a"
+        fsm = RegexFSM(pattern)
+        test_string = "a"
+        result = fsm.check_string(test_string)
+        self.assertTrue(result, f"Expected True for pattern '{pattern}' and string '{test_string}'")
+
+
+    def test_simple_pattern1(self):
+        pattern = "a*b"
+        fsm = RegexFSM(pattern)
+        test_string = "aaab"
+        result = fsm.check_string(test_string)
+        self.assertTrue(result, f"Expected True for pattern '{pattern}' and string '{test_string}'")
+
+
+    def test_no_match(self):
+        pattern = "a*b"
+        fsm = RegexFSM(pattern)
+        test_string = "aaac"
+        result = fsm.check_string(test_string)
+        self.assertFalse(result, f"Expected False for pattern '{pattern}' and string '{test_string}'")
+
+    def test_complex_pattern1(self):
+        pattern = "a*b.c+"
+        fsm = RegexFSM(pattern)
+        test_string = "aaabxc"
+        result = fsm.check_string(test_string)
+        self.assertTrue(result, f"Expected True for pattern '{pattern}' and string '{test_string}'")
+    def test_complex_pattern1_no_match(self):
+        pattern = "a*b.c+"
+        fsm = RegexFSM(pattern)
+        test_string = "aaabx"
+        result = fsm.check_string(test_string)
+        self.assertFalse(result, f"Expected False for pattern '{pattern}' and string '{test_string}'")
+
+    def test_stars_pattern1_1(self):
+        pattern = "a*a*a*"
+        fsm = RegexFSM(pattern)
+        test_string = "aaaa"
+        result = fsm.check_string(test_string)
+        self.assertTrue(result, f"Expected True for pattern '{pattern}' and string '{test_string}'")
+    def test_stars_pattern1_2(self):
+        pattern = "a*a*a*"
+        fsm = RegexFSM(pattern)
+        test_string = "bbbbb"
+        result = fsm.check_string(test_string)
+        self.assertTrue(result, f"Expected False for pattern '{pattern}' and string '{test_string}'")
+    def test_plus_pattern1_1(self):
+        pattern = "a+b+c+"
+        fsm = RegexFSM(pattern)
+        test_string = "aaabbbccc"
+        result = fsm.check_string(test_string)
+        self.assertTrue(result, f"Expected True for pattern '{pattern}' and string '{test_string}'")
+    def test_plus_pattern1_1_no_match(self):
+        pattern = "a+b+c+"
+        fsm = RegexFSM(pattern)
+        test_string = "aaacc"
+        result = fsm.check_string(test_string)
+        self.assertFalse(result, f"Expected False for pattern '{pattern}' and string '{test_string}'")
+
+    def test_plus_pattern1_2(self):
+        pattern = "a+kb+c+"
+        fsm = RegexFSM(pattern)
+        test_string = "akbbbccccccc"
+        result = fsm.check_string(test_string)
+        self.assertTrue(result, f"Expected False for pattern '{pattern}' and string '{test_string}'")
+
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
